dffcafe3373657fe14a6d3cb1451e51341d748d9
kasope_johnson <kj@gtco.net>
2023-10-21 23:39:51 +0100

    fix: defines very basic style, adds dark/light theme support

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,50 @@
-<script lang="ts" setup></script>
-
 <template>
   <main class="layout default">
+    <header>
+      <nav class="container">
+        <div class="brand">
+          <span class="brand-name"> Kasope </span>
+        </div>
+        <ul class="nav-menu">
+          <li class="nav-item"><a href="#" class="nav-link">home</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">works</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">about-me</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">contacts</a></li>
+        </ul>
+      </nav>
+    </header>
     <slot />
   </main>
 </template>
 
-<style scoped lang="scss"></style>
+<script setup lang="ts">
+  function changeTheme() {
+    if (document.documentElement.getAttribute("data-theme") !== "light") {
+      document.documentElement.setAttribute("data-theme", "light");
+    } else document.documentElement.removeAttribute("data-theme");
+  }
+</script>
+
+<style scoped lang="scss">
+  main {
+    > * {
+      width: 100%;
+    }
+  }
+  nav {
+    margin: auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 32px 0 8px 0;
+  }
+  ul.nav-menu {
+    list-style: none;
+    li {
+      display: inline;
+      &:not(:first-child) {
+        margin-left: 22px;
+      }
+    }
+  }
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   alias: {
     'styles': resolve('./assets/styles'),
   },
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['styles/scss/index.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "styles/scss/_colors.scss" as *;'
+        }
+      }
+    }
+  }
 })
